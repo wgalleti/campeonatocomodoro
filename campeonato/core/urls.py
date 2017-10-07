f15b6efc36d154be557b44fa4e20f79c10335f2f
William Galleti <william.galleti@gmail.com>
2017-10-07 11:11:12 -0300

    Implementado frontend

--- campeonato/core/urls.py
+++ campeonato/core/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
-from .views import sorteio
+from .views import *
 
 urlpatterns = [
+    url(r'^$', index, name='index'),
+    url(r'^inscricao/', inscricao, name='inscricao'),
     url(r'^sorteio/', sorteio, name='sorteio'),
 ]
--- campeonato/core/views.py
+++ campeonato/core/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .services import SorteioService
+from .models import Combinacao
 
 # Create your views here.
 
+
+def index(request):
+    return render(request, 'home.html')
+
+def inscricao(request):
+    c = Combinacao.objects.all().order_by('idade', 'faixa', 'peso')
+    return render(request, 'chaves.html', {'combinacoes': c})
 
 def sorteio(request):
 
